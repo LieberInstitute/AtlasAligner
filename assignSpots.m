9a547d6b0bf70d02f2170a395155f16b10055451
joecat6 <j.l.cat.6@gmail.com>
2020-06-28 17:18:22 -0400

    add some comments

--- assignSpots.m
+++ assignSpots.m
@@ -1,27 +1,48 @@
 function tissuePositions = assignSpots(mask, tissuePositions, maskName, append)
 
+    % This is a function to assign spots to sub-regions identified in this
+    % tool. 
+    % Inputs:
+    % mask: labeled mask sized to the same size as the histology image
+    % tissuePositions: table object from tissue_positions_list of spot
+    % coordinates 
+    % maskName: name of the mask file (used for saving)
+    % append: logical of whether to append regions to tissuePositions or
+    % overwrite
+    % Output:
+    % tissuePositions: table object with regions and mask labels added
 
+    % Number of spots
     nSpots = size(tissuePositions, 1);
+    % row and column coordinates
     crow = table2array(tissuePositions(:, 5));
     ccol = table2array(tissuePositions(:, 6));
     if append
+        % get previous regions
         region = table2array(tissuePositions(:, 7));
+        % get previous mask labels
         maskLabel = table2array(tissuePositions(:, 8));
+        % if not read in as cell array, convert region to cell array
         if ~isa(region, 'cell')
             region = num2cell(region);
         end
+        % for each spot append region and mask label to previous region and
+        % mask label, separated by '\'
         for i = 1:nSpots
             region{i} = [num2str(region{i}) '\' num2str(mask(crow(i), ccol(i)))];
             maskLabel{i} = [maskLabel{i} '\' maskName];
         end
+        % replace region and mask label with appended values
         tissuePositions = [tissuePositions(:,1:6) array2table(region) array2table(maskLabel)];
     else
         region = cell(nSpots, 1);
         maskLabel = cell(nSpots, 1);
+        % for each spot assign region and mask label 
         for i = 1:nSpots
             region{i} = num2str(mask(crow(i), ccol(i)));
             maskLabel{i} = maskName;
         end
+        % add region and mask label columns to tissuePositions table
         tissuePositions = [tissuePositions array2table(region) array2table(maskLabel)];
     end
     
--- makeSubRegions.m
+++ makeSubRegions.m
@@ -1,20 +1,43 @@
 function makeSubRegions(atlasPathFull, nRegions)
 
+    % This is a function to create a labeled mask from a atlas image from
+    % the <name of atlas> mouse brain atlas.
+    % Inputs:
+    % atlasPathFull: full path to image from the <name of atlas> mouse
+    % brain atlas
+    % nRegions: the number of regions to identify from the atlas
+
+    % read in atlas image
     atlas = imread(atlasPathFull);
     [path, name, ~] = fileparts(atlasPathFull);
+    % red channel
     atlas_R = atlas(:,:,1);
+    % blue channel
     atlas_B = atlas(:,:,3);
+    % modified atlas image: subtract red from blue, borders of regions 
+    % should be white and interior black
     im = double(atlas_B-atlas_R);
+    % make figure
     f = figure('units', 'normalized', 'position', [0 0 1 1]);
+    % display atlas image
     ax = imagesc(atlas); colormap gray; axis image;
+    % create mask object
     mask = zeros(size(im));
+    % set mask as UserData for axis and figure
     set(ax, 'UserData', mask);
     set(f, 'UserData', mask);
-    uipanel('units', 'normalized', 'position', [0.095 0.005 0.18 0.05], 'Title', 'Sub Region Assignment');
-    assignDialog = uicontrol('Style', 'edit', 'units', 'normalized', 'position', [0.15 0.01 0.075 0.02], 'String', 1);
-    assignDown = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.1 0.01 0.03 0.02], 'String', '-');
-    assignUp = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.24 0.01 0.03 0.02], 'String', '+');
+    % panel for region assignment
+    uipanel('units', 'normalized', 'position', [0.095 0.005 0.18 0.05],...
+        'Title', 'Region Assignment');
+    % dialog and buttons for region assignment
+    assignDialog = uicontrol('Style', 'edit', 'units', 'normalized',...
+        'position', [0.15 0.01 0.075 0.02], 'String', 1);
+    assignDown = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.1 0.01 0.03 0.02], 'String', '-');
+    assignUp = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.24 0.01 0.03 0.02], 'String', '+');
     assign = str2double(get(assignDialog, 'String'));
+    % disable up/down buttons if they reach upper/lower limits
     if assign==1
         set(assignDown, 'Enable', 'off');
     elseif assign==nRegions
@@ -23,57 +46,96 @@ function makeSubRegions(atlasPathFull, nRegions)
         set(assignDown, 'Enable', 'on');
         set(assignUp, 'Enable', 'on');
     end
+    % callback functions for changing region assignment
     assignDialog.Callback = {@changeAssign, assignDown, assignUp, assign, nRegions};
     assignDown.Callback = {@assignDownFcn, assignUp, assignDialog, nRegions};
     assignUp.Callback = {@assignUpFcn, assignDown, assignDialog, nRegions};
-    uipanel('units', 'normalized', 'position', [0.28 0.005 0.18 0.045], 'Title', 'Dilation');
-    jumpDialog = uicontrol('Style', 'edit', 'units', 'normalized', 'position', [0.335 0.01 0.06 0.02], 'String', 1);
-    jumpDown = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.285 0.01 0.03 0.02], 'String', '-');
-    jumpUp = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.415 0.01 0.03 0.02], 'String', '+');
+    % panel for dilation of modified atlas image
+    uipanel('units', 'normalized', 'position', [0.28 0.005 0.18 0.045],...
+        'Title', 'Dilation');
+    % dialog and buttons for dilation
+    jumpDialog = uicontrol('Style', 'edit', 'units', 'normalized',...
+        'position', [0.335 0.01 0.06 0.02], 'String', 1);
+    jumpDown = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.285 0.01 0.03 0.02], 'String', '-');
+    jumpUp = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.415 0.01 0.03 0.02], 'String', '+');
     jump = str2double(get(jumpDialog, 'String'));
+    % disable up/down buttons if they reach upper/lower limits
     if jump==0
         set(jumpDown, 'Enable', 'off');
     else
         set(jumpDown, 'Enable', 'on');
         set(jumpUp, 'Enable', 'on');
     end
+    % callback functions for changing dilation
     jumpDialog.Callback = {@changeJump, jumpDown, jumpUp, jump};
     jumpDown.Callback = {@jumpDownFcn, jumpUp, jumpDialog};
     jumpUp.Callback = {@jumpUpFcn, jumpDown, jumpDialog};
+    % make colormap
     cmap = hsv(nRegions);
+    % click function for region assignment
     set(ax, 'ButtonDownFcn', {@click, f, im, assignDialog, jumpDialog, cmap});
-    polyButton = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.5 0.01 0.05 0.045], 'String', 'ROI Poly');
+    % button and callback for roiPoly region assignment
+    polyButton = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.5 0.01 0.05 0.045], 'String', 'ROI Poly');
     polyButton.Callback = {@polyTool, f, atlas, assignDialog, cmap, im, jumpDialog};
-    saveButton = uicontrol('Style', 'push', 'units', 'normalized', 'position', [0.9 0.01 0.05 0.045], 'String', 'Save');
+    % button and callback function for saving mask
+    saveButton = uicontrol('Style', 'push', 'units', 'normalized',...
+        'position', [0.9 0.01 0.05 0.045], 'String', 'Save');
     saveButton.Callback = {@saveManual, f, fullfile(path, name)};
     hold on;
 
 end
 
 function click(hObj, Event, f, im, assignDialog, jumpDialog, cmap)
+
+    % This is the click function for region assignment
+    % Inputs:
+    % hObj: axis or plot object
+    % Event: click information
+    % f: figure
+    % im: modified atlas image
+    % assignDialog: dialog to get region number
+    % jumpDialog: dialog to get dilation 
+    % cmap: colormap
     
+    % dilation
     jump = str2double(get(jumpDialog, 'String'));
+    % region number
     assign = str2double(get(assignDialog, 'String'));
+    % dilate modified image
     se = strel('disk', jump);
     im = imdilate(im, se);
+    % get coordinates of click
     x = floor(Event.IntersectionPoint(1));
     y = floor(Event.IntersectionPoint(2));
+    % get mask from UserData
     mask = get(f, 'UserData');
+    % if click occurred in already defined region
     if mask(y,x) > 0
-        maskLab = bwlabel(mask);
-        idx = maskLab(y,x);
+        % get region idx of clicked area
+        idx = mask(y,x);
         delete(hObj);
-        mask(maskLab==idx)=0;
+        % set clicked region to zero
+        mask(mask==idx)=0;
+        % set mask as figure UserData
         set(f, 'UserData', mask);
     else
+        % region grow from clicked coordinate on modified atlas image
         tmp = imfill(regiongrowing(im, y, x, 0), 'holes');
+        % assign region grown area to assign
         mask(tmp==1) = assign;
-        %figure; imshow(mask);
+        % set mask as figure data
         set(f, 'UserData', mask);
+        % get coordinates of new region
         [c,r] = find(mask==assign);
+        % plot region coordinates as color
         hold on;
-        hLine = scatter(r, c, 25, cmap(assign,:), 'filled', 'ButtonDownFcn', {@click, f, im, assignDialog, jumpDialog, cmap});
+        hLine = scatter(r, c, 25, cmap(assign,:), 'filled', ...
+            'ButtonDownFcn', {@click, f, im, assignDialog, jumpDialog, cmap});
         hold off;
+        % set mask as UserData of plot object
         set(hLine, 'UserData', mask);
     end
 end
